f4c1fd0001733024535e1d482f7fa9a0d1af007d
Juan Herrera <me@jok3r.site>
2025-03-30 23:56:52 -0600

    Update 03_pattern.py
    a complicated way to print the pattern using for instead print many times

--- 1-hello-world/03_pattern.py
+++ 1-hello-world/03_pattern.py
@@ -1,4 +1,14 @@
-print('       1')
-print('     2 3')
-print('   4 5 6')
-print('7 8 9 10')
+# Write code below 💖
+
+current_number = 1
+
+for line in range(1, 5): // here a fancy way to write 4 
+    spaces = max(6 - 2 * (line - 1), 0) (we calculate the spaces on each line) 
+    
+    row = " " * spaces
+    
+    for i in range(line): (inception for other for to add the numbers depend the line, if line is equal 1, so current_number is 1 stop and star again. then if line 2 so add two numbers and start un 2 because the previous += add 1 to curren_number so 2, again 3 ) 
+        row += " " + str(current_number)
+        current_number += 1
+    
+    print(row)
